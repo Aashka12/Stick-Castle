d2e9c57f6ea0a2430c0dbf7c86f6fd7588c0a8ef
Aashka12 <63451066+Aashka12@users.noreply.github.com>
2020-04-29 23:31:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  rect (160,200,70,350);
+  rect (140,160,40,390);
+  rect (230,160,40,390);
+  rect (100,130,40,420);
+  rect (270,130,40,420);
+  rect (70,70,20,450);
+  rect (320,70,20,450);
+  triangle(230,200,205,170,180,200);
+  ellipse(120,110,40);
+  ellipse(290,110,40);
+}
